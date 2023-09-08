58ab6bd2c72d2cbb61b142d3214bbaa794f6d9cf
Okononfua Mathew <oseghaleokononfua@gmail.com>
2023-09-08 10:19:55 +0100

    main

--- app.js
+++ app.js
@@ -6,12 +6,19 @@ const port = 3000;
 
 app.get('/api', (req, res) => {
   const { slack_name, track } = req.query;
-  
+
   // Get the current time in the Nigerian time zone (UTC+1)
   const nigerianTime = DateTime.now().setZone('Africa/Lagos');
 
-  const formattedUtcTime = nigerianTime.toFormat('yyyy-MM-dd\'T\'HH:mm:ss\'Z\'');
-  
+  // Calculate a random offset within the +/-2 minute window
+  const randomOffset = Math.random() * 4 - 2; // Random value between -2 and 2
+  const offsetMilliseconds = randomOffset * 60 * 1000; // Convert minutes to milliseconds
+
+  // Apply the offset to the current UTC time
+  const accurateUtcTime = new Date(nigerianTime.toJSDate().getTime() - offsetMilliseconds);
+
+  const formattedUtcTime = accurateUtcTime.toISOString();
+
   // Prepare the response
   const response = {
     slack_name: slack_name,
@@ -20,7 +27,7 @@ app.get('/api', (req, res) => {
     track: track,
     github_file_url: 'https://github.com/Mathew-Pio/HNG-PRJECT1/blob/main/app.js',
     github_repo_url: 'https://github.com/Mathew-Pio/HNG-PRJECT1',
-    status_code:'200'
+    status_code: '200'
   };
 
   // Send the JSON response
